2c2c50ef6fc1e44eafb581272267f1f751a03af2
Django L. Kerger <djangol.kerger@MacBook-Pro.attlocal.net>
2020-06-29 09:39:17 -0500

    starting again monday morning

--- app.js
+++ app.js
@@ -1,9 +1,69 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https")
 
 const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+    res.sendfile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req,res){
+
+    const firstName = req.body.fname
+    const lastName = req.body.lname
+    const email = req.body.email
+
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    var jsonData = JSON.stringify(data);
+
+    const url = "https://us10.api.mailchimp.com/3.0/lists/9d60eb8b00"
+
+    const options = {
+        method: "POST",
+        auth: "django1:a7b7a1b133eec7c1ce2bc626229cf80e-us10"
+
+    }
+
+    const request = https.request(url, options, function(response){
+        
+        if (response.statusCode === 200) {
+            res.send("Successfully subscribed")
+        } else {
+            res.send("there was an error please try again")
+        }
+        
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+
+    request.end();
+    
+});
 
 app.listen(3000, function(){
     console.log("server is running");
 });
+//api key
+//a7b7a1b133eec7c1ce2bc626229cf80e-us10
+//listid
+//9d60eb8b00
